241733b5f74bafb3364796e3b9595f1f7cfbb126
Bunnyspa <bunnyspa242@gmail.com>
2020-10-15 20:27:43 +0900

    7.2.4

--- src/main/App.java
+++ src/main/App.java
@@ -28,7 +28,7 @@ public class App {
 
     public static final String NAME_KR = "소녀전선 칩셋 조합기";
     public static final String NAME_EN = "Girls' Frontline HOC Chip Calculator";
-    public static final Version3 VERSION = new Version3(7, 2, 3);
+    public static final Version3 VERSION = new Version3(7, 2, 4);
     private static final String RESOURCE_PATH = "/resources/";
 
     public Color orange() {
